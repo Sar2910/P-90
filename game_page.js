56f9c7696b24e72ef18e474b15516d28444c6758
Sar2910 <90505917+Sar2910@users.noreply.github.com>
2022-06-24 20:40:32 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,6 @@
+player1 = localStorage.getItem("player1_name");
+player2 = localStorage.getItem("player2_name");
+player1_score = 0;
+player2_score = 0;
+document.getElementById("player1_name").innerHTML=player1;
+document.getElementById("player2_name").innerHTML=player2;
